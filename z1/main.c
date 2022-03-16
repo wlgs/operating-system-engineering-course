0977d5f65dee2bb952a66102188becbfe036947b
Mikołaj Wielgos <wielgosmikolaj@gmail.com>
2022-03-16 10:03:10 +0100

    another try

--- z1/main.c
+++ z1/main.c
@@ -2,20 +2,18 @@
 #include "libz1.h"
 #include <stdlib.h>
 #include <unistd.h>
-#include <time.h>
+#include <sys/resource.h>
 
-double timeDifference(clock_t t1, clock_t t2){
-    return ((double)(t2 - t1) / sysconf(_SC_CLK_TCK));
+float diffUserTime(struct rusage *start, struct rusage *end)
+{
+    return (end->ru_utime.tv_sec - start->ru_utime.tv_sec) +
+           1e-6*(end->ru_utime.tv_usec - start->ru_utime.tv_usec);
 }
 
-void writeResult(FILE* resFile, clock_t start, clock_t end, struct tms* t_start, struct tms* t_end){
-    printf("\tREAL_TIME: %fl\n", timeDifference(start,end));
-    printf("\tUSER_TIME: %fl\n", timeDifference(t_start->tms_utime, t_end->tms_utime));
-    printf("\tSYSTEM_TIME: %fl\n", timeDifference(t_start->tms_stime, t_end->tms_stime));
-
-    fprintf(resFile, "\tREAL_TIME: %fl\n", timeDifference(start, end));
-    fprintf(resFile, "\tUSER_TIME: %fl\n", timeDifference(t_start->tms_utime, t_end->tms_utime));
-    fprintf(resFile, "\tSYSTEM_TIME: %fl\n", timeDifference(t_start->tms_stime, t_end->tms_stime));
+float diffSystemTime(struct rusage *start, struct rusage *end)
+{
+    return (end->ru_stime.tv_sec - start->ru_stime.tv_sec) +
+           1e-6*(end->ru_stime.tv_usec - start->ru_stime.tv_usec);
 }
 
 int main(int argc, char **argv)
