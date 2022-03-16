88ac29e19220427bb181b38c129ce9176acbf246
Mikołaj Wielgos <wielgosmikolaj@gmail.com>
2022-03-16 09:46:18 +0100

    upd

--- z1/main.c
+++ z1/main.c
@@ -1,22 +1,6 @@
 #include <stdio.h>
 #include "libz1.h"
 #include <stdio.h>
-#include <unistd.h>
-#include <sys/times.h>
-
-double timeDifference(clock_t t1, clock_t t2){
-    return ((double)(t2 - t1) / sysconf(_SC_CLK_TCK));
-}
-
-void writeResult(FILE* resFile, clock_t start, clock_t end, struct tms* t_start, struct tms* t_end){
-    printf("\tREAL_TIME: %fl\n", timeDifference(start,end));
-    printf("\tUSER_TIME: %fl\n", timeDifference(t_start->tms_utime, t_end->tms_utime));
-    printf("\tSYSTEM_TIME: %fl\n", timeDifference(t_start->tms_stime, t_end->tms_stime));
-
-    fprintf(resFile, "\tREAL_TIME: %fl\n", timeDifference(start, end));
-    fprintf(resFile, "\tUSER_TIME: %fl\n", timeDifference(t_start->tms_utime, t_end->tms_utime));
-    fprintf(resFile, "\tSYSTEM_TIME: %fl\n", timeDifference(t_start->tms_stime, t_end->tms_stime));
-}
 
 int main(int argc, char **argv)
 {
@@ -69,7 +53,7 @@ int main(int argc, char **argv)
         }
         else
         {
-            printf("Bad argument");
+            printf("Bad argument\n");
             return -1;
         }
     }
