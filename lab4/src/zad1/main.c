e8c09c99689a0a454c69fb63c346e1fce85ebbe7
Mikołaj Wielgos <wielgosmikolaj@gmail.com>
2022-04-08 23:01:07 +0200

    lab4 zad1 done, lab5 some work

--- lab4/src/zad1/main.c
+++ lab4/src/zad1/main.c
@@ -1,8 +1,68 @@
 #include <stdio.h>
+#include <string.h>
+#include <signal.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <wait.h>
 
+const int SIGNAL_ID = SIGUSR1;
 
-int main(int argc, char *argv[]){
+void signalHandle(int sigtype) {
+    printf("Signal received: %d\n", sigtype);
+}
 
+void maskSignal() {
+    sigset_t blocked_set;
+    sigemptyset(&blocked_set);
+    sigaddset(&blocked_set, SIGNAL_ID);
+    sigprocmask(SIG_BLOCK, &blocked_set, NULL);
+}
 
+void checkSignalPending() {
+    sigset_t pending_set;
+    sigpending(&pending_set);
+    if (sigismember(&pending_set, SIGNAL_ID))
+        printf("Signal with id %d is pending.\n", SIGNAL_ID);
+    else
+        printf("Signal with id %d is NOT pending.\n", SIGNAL_ID);
+}
+
+int main(int argc, char **argv) {
+    argv++;
+    argc--;
+    if (argc != 1) {
+        printf("Wrong amount of arguments.");
+        return -1;
+    }
+    char *command = argv[0];
+    if (strcmp(command, "ignore") == 0) {
+        signal(SIGNAL_ID, SIG_IGN);
+    } else if (strcmp(command, "handler") == 0) {
+        signal(SIGNAL_ID, signalHandle);
+    } else if (strcmp(command, "mask") == 0 || strcmp(command, "pending") == 0) {
+        maskSignal();
+    }
+    printf("Trying to raise the signal...\n");
+    raise(SIGNAL_ID);
+
+    printf("Main process: ");
+    checkSignalPending();
+
+    int pid = fork();
+    if (pid == 0) //child
+    {
+        if (strcmp(command, "pending") == 0) {
+            printf("[CHILD] Trying to raise the signal...\n");
+            raise(SIGNAL_ID);
+        }
+        printf("Child process: ");
+        checkSignalPending();
+        if (execl("./main2", "main2", NULL) == -1) {
+            fprintf(stderr, "Error executing main2");
+            exit(1);
+        }
+    }
+
+    wait(NULL);
     return 0;
 }
--- lab5/src/zad1/main.c
+++ lab5/src/zad1/main.c
@@ -1,8 +1,38 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 
 int main(int argc, char *argv[]){
 
+    argc--;
+    argv++;
 
+    if (argc != 1) {
+        printf("Incorrect amount of arguments.");
+        return -1;
+    }
+    char* line = calloc(128, sizeof(char));
+    FILE *fp = fopen(argv[0], "r");
+    if (!fp){
+        fprintf(stderr, "Error opening file '%s'\n", argv[0]);
+        return -1;
+    }
+    char sep = '=';
+    char** execStrings = calloc(128, sizeof(char*));
+    char* command = calloc(128, sizeof(char));
+    while(fgets(line, 128,fp)){
+        printf("The line: %s", line);
+        char* ptr = strstr(line, &sep);
+        if (!ptr){
+            continue;
+        }
+        ptr++;
+        ptr++;
+        printf("found %s\n", ptr);
+        free(ptr);
+    }
+
+    fclose(fp);
     return 0;
 }
